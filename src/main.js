68d110404df0032a9e478dc35ad6929cdef28404
emilianovargas13 <83984657+emilianovargas13@users.noreply.github.com>
2024-12-13 20:42:18 -0600

    #9

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import './registerServiceWorker';
-
+import './service-worker'
 
 // Font Awesome
 import { library } from "@fortawesome/fontawesome-svg-core";
